741b8fea2a982893af902fa79eecc80336aa2e84
lakshmanvpg <lakshminarayanareddyv2@gmail.com>
2026-09-14 22:24:40 +0530

    Add infrastructure configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,11 @@
 resource "aws_s3_bucket" "application" {
-  bucket = "github-actions-${var.environment}-143341541"
+
+  bucket = "github-actions-${lower(var.environment)}-1433415421"
 
   tags = {
     Name        = "github-actions-${var.environment}"
     Environment = var.environment
     Project     = "Scenario-3-CICD"
   }
+
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "region" {
+  description = "AWS deployment region"
+  type        = string
+  default     = "us-east-1"
+}
